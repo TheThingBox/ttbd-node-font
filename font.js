c372a309b50ffbc5163625082e0a5497c89cc896
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-05-31 16:54:07 +0200

    init

---
+++ font.js
@@ -0,0 +1,43 @@
+module.exports = function(RED) {
+  "use strict";
+
+  function tsa_font(n) {
+    RED.nodes.createNode(this, n);
+    this.font = n.font;
+    this.font_type = n.font_type
+    var node = this;
+
+    this.on("input", function(msg) {
+      msg.font = node.font + (this.font_type || "");
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("tsa_font", tsa_font);
+
+  RED.httpAdmin.get("/font/list", function(req, res) {
+    var fonts = {
+      "arial": {
+        "Classic": "",
+        "Bold": "bd",
+        "Italic": "i",
+        "Bold italic": "bi"
+      },
+      "Roboto": {
+        "Classic": "-Regular",
+        "Bold": "-Bold",
+        "Italic": "-Italic",
+        "Light": "-Light"
+      },
+      "Times": {
+        "Classic": "",
+        "Bold": "bd",
+        "Italic": "i",
+        "Bold italic": "bi"
+      },
+      "Ubuntu": {
+        "Classic": "-C",
+      }
+    }
+    res.json(200, { fonts: fonts });
+  })
+}
